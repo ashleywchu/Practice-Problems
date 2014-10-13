b960594fe314f5d9d3ada6f9e77ff04b45a837b0
pillagedeyes <Achu@Ashley-Chus-MacBook-Pro.local>
2014-10-12 23:38:37 -0700

    RPS

---
+++ RockPaperScissors.rb
@@ -0,0 +1,17 @@
+def rps
+	computer_move = ["Rock", "Paper", "Scissors"]
+	computer = computer_move.sample
+
+	puts "Rock, Paper, or Scissors?"
+	me = gets.chomp
+	puts "Your move is #{me}. The computer's move is #{computer}."
+	
+	if me == computer
+		puts "It's a tie!"
+	elsif me != computer
+		lose = "You lose."
+		win = "You win!"
+		puts lose if me == "Rock" && computer == "Paper" || me == "Paper" && computer == "Scissors" || me == "Scissors" && computer == "Rock"
+		puts win if me == "Rock" && computer == "Scissors" || me == "Paper" && computer == "Rock" || me == "Scissors" && computer == "Paper"
+	end
+end
